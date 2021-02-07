23e372e04e3610e281908b8b5e2338782f345029
Thevic16 <20171408@ce.pucmm.edu.do>
2021-02-07 19:35:08 -0400

    Agregando try catch

--- src/main/java/edu/pucmm/eict/Main.java
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -4,30 +4,34 @@ import edu.pucmm.eict.util.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
-            String uri = "https://avathartech.com/";
-            Document doc = Jsoup.connect(uri).get();
+            try {
+                String uri = "https://avathartech.com/";
+                Document doc = Jsoup.connect(uri).get();
 
-            //Ejecutando operacion A.
-            //new OperacionA(uri).EjecutarOperacion();
+                //Ejecutando operacion A.
+                //new OperacionA(uri).EjecutarOperacion();
 
-            //Ejecutando operacion B.
-            //new OperacionB(doc).EjecutarOperacion();
+                //Ejecutando operacion B.
+                //new OperacionB(doc).EjecutarOperacion();
 
-            //Ejecutando operacion C.
-            //new OperacionC(doc).EjecutarOperacion();
+                //Ejecutando operacion C.
+                //new OperacionC(doc).EjecutarOperacion();
 
-            //Ejecutando operacion D.
-            //new OperacionD(doc).EjecutarOperacion();
+                //Ejecutando operacion D.
+                //new OperacionD(doc).EjecutarOperacion();
 
-            //Ejecutando operacion E.
-            new OperacionE(doc).EjecutarOperacion();
+                //Ejecutando operacion E.
+                new OperacionE(doc).EjecutarOperacion();
+
+            } catch (Exception e){
+                System.out.println(e);
+                System.out.println("Algo salio mal....");
+            }
+
     }
 
 }
